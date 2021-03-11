dd34dafbe7fda0bda2b83780b82ba265e2007598
webtechandbiz <76784296+webtechandbiz@users.noreply.github.com>
2021-03-11 14:05:58 +0100

    Update -functions-importer-read.php

--- -functions-stogidrawr/-functions-importer-read.php
+++ -functions-stogidrawr/-functions-importer-read.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function get_feed($ws_url__importer, $format__importer){
     
     switch ($format__importer) {
@@ -11,6 +10,14 @@ function get_feed($ws_url__importer, $format__importer){
             $sXML = _get_feed_xml($_auth, $ws_url__importer);
 
             return $sXML;
+
+        case '2': //CSV
+            $_auth['username'] = get_option('username_basicauth__importer');
+            $_auth['password'] = get_option('password_basicauth__importer');
+
+            $sCSV = _get_feed_csv($_auth, $ws_url__importer);
+
+            return $sCSV;
 
         default:
             break;
