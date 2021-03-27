40e389d18e4f00a6910617d30fd0f8e3957e5b93
webtechandbiz <76784296+webtechandbiz@users.noreply.github.com>
2021-03-27 13:22:51 +0100

    Update -functions.php

--- -functions-stogidrawr/-functions.php
+++ -functions-stogidrawr/-functions.php
@@ -1,2 +1,11 @@
 <?php
 //# Basic functions
+function _get_import_code() {
+    $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    $charactersLength = strlen($characters);
+    $importcode = '';
+    for($i=0;$i<10;$i++){
+        $importcode .= $characters[rand(0, $charactersLength - 5)];
+    }
+    return $importcode;
+}
